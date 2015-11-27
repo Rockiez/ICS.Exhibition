e592364e7054570703694c3021fa0f0d596386b6
rockiez <simpi@foxmail.com>
2015-11-27 23:13:06 +0800

    the first commit.

---
+++ ICS.Exhibition/Global.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICS.Acquisition;
+
+namespace ICS.Exhibition
+{
+    public static class Global
+    {
+        public static ADAM4150 ADAM4150Provider
+        {
+            get { return (ADAM4150) ClassFactory.GetProvider(equipmentCategory.ADAM4150, ComSetting); }
+        }
+
+        public static ICS.Models.Com.ComSettingModel _ComSetting;
+
+        public static ICS.Models.Com.ComSettingModel ComSetting
+        {
+            get
+            {
+                if (_ComSetting == null)
+                {
+                    _ComSetting = new ICS.Common.SettingHelper<ICS.Models.Com.ComSettingModel>().GetSetting();
+                    _ComSetting.DigitalQuantityCom = "Com2";
+                    _ComSetting.AnalogQuantityCom = "Com2";
+                }
+                return _ComSetting;
+            }
+        }
+    }
+}
---
+++ ICS.Exhibition/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using ICS.Acquisition;
+using ICS.Models;
+using ICS.Common;
+
+namespace ICS.Exhibition
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void minbottor(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+            
+        }
+
+        private void Closebotter(object sender, RoutedEventArgs e)
+        {
+            if (MessageBoxResult.Yes !=
+                MessageBox.Show("你确定要退出？", "退出", MessageBoxButton.YesNo, MessageBoxImage.Asterisk)) return;
+            Close();
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+                var exHelper = new ExhibitionHelper(exhibitionLight, catchThief, policemen, haveThief, this, imgDoor.Name, imgDoor.Name);
+
+                LazyTimer timer = new LazyTimer( _sender =>
+                {
+                    LazyTimer t = (LazyTimer)_sender[0];
+
+                    if (Global.ADAM4150Provider.CheckSerialPort(Global.ADAM4150Provider.ADAM4017Provider).Status ==
+                        RunStatus.Failure) return;
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        exHelper.OpenAlarm(Global.ADAM4150Provider.infraredValue);
+
+                        t.Reset();
+                    });
+                }, 100, 5000);
+                exHelper.BindImageMargin(girdMain.Children);
+            }
+    }
+}
